e28bef451c15d7f94d2f93c75daf96c96867a28c
Drew Bollinger <drew.bollinger@gsa.gov>
2023-08-16 10:41:45 -0400

    update to react 18, router v6

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
+
+import { createRoot } from 'react-dom/client';
 import React from "react";
-import ReactDOM from "react-dom";
-import Main from "./Main";
- 
-ReactDOM.render(
-  <Main/>, 
-  document.getElementById("root")
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+  } from "react-router-dom";
+  import Home from "./Home";
+  import Stuff from "./Stuff";
+  import Contact from "./Contact";
+  import Layout from "./Layout" 
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />}/>
+      <Route path="/stuff" element={<Stuff />}/>
+      <Route path="/contact" element={<Contact />}/>
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
