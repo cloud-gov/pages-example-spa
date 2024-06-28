39bd28a243355469ccc64b1e082b21fac92b3cda
Ephraim-G <ephraim.gross@gsa.gov>
2024-06-28 15:03:25 -0400

    add BASEURL env var

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: process.env.BASEURL,
   build: {
     outDir: '_site'
   }
