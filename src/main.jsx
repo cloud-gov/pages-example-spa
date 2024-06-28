95d68d3011b3a809046bb14924d55658203c6765
Ephraim-G <ephraim.gross@gsa.gov>
2024-06-28 15:49:43 -0400

    update env var

--- src/main.jsx
+++ src/main.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter(
       <Route path="*" element={<Page404 />}/>
     </Route>
   ),
-  {basename: process.env.SITE_PREFIX}
+  {basename: import.meta.env.BASE_URL}
 );
 
 createRoot(document.getElementById("root")).render(
